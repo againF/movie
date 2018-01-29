cfc6ade6bcdde89233054170452d1e79265a382f
againF <wyffcld@163.com>
2018-01-29 18:16:59 +0800

    grunt集成自动重启

---
+++ gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            jade: {
+                files: ['views/**'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['public/js/**', 'models/**/*.js', 'schemas/**/*.js'],
+                //tasks: ['jshint'],
+                options: {
+                    livereload: true                    
+                }
+            }
+        },
+
+        nodemon: {
+            dev: {
+                script: 'app.js',
+                options: {
+                    //file: 'app.js',//入口文件
+                    args: [],
+                    ignoredFiles: ['README.md', 'node_modules/**', '.vscode/**', '使用说明.doc'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['./'],
+                    debug: true,
+                    delayTime: 1,//大量文件改动时不用每个改动都重启服务而是延迟几毫秒去重启服务
+                    env: {
+                        PORT: 4000
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
+
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-nodemon');//用于自动重启app.js
+    grunt.loadNpmTasks('grunt-concurrent');//针对慢任务开发
+
+    grunt.option('force', true);//防止因为语法错误中断整个grunt服务
+    grunt.registerTask('default', ['concurrent']);
+    
+}

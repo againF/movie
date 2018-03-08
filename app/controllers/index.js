5e015557da5c32201f5709bd812e69ee478b39b1
againF <wyffcld@163.com>
2018-03-08 17:19:59 +0800

    增加分类列表及分页

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,3 +15,32 @@ exports.index = function(req, res) {
             })
         })
 };
+
+//search page
+exports.search = function(req, res) {
+    var catId = req.query.cat;
+    var page = parseInt(req.query.p, 10);
+    var count = 2;
+    var index = page * count;//每项展示数据条数
+    Category
+        .find({_id: catId})
+        .populate({
+            path: 'movies'
+        })
+        .exec(function(err, categories) {
+            if(err) {
+                console.log(err);
+            }
+            var category = categories[0] || {};
+            var movies = category.movies || [];
+            var results = movies.slice(index, index + count);
+            res.render('results', {
+                title: 'movie 结果列表页',
+                movies: results,
+                currentPage: (page + 1),
+                totalPage: Math.ceil(movies.length / 2),
+                query: 'cat=' + catId,
+                keyword: category.name
+            })
+        })
+};
--- config/routes.js
+++ config/routes.js
@@ -38,4 +38,6 @@ module.exports = function(app) {
     app.post('/admin/category/new', User.signinRequired, User.adminRequired, Category.save)
     app.get('/admin/category/list', User.signinRequired, User.adminRequired, Category.list);
     
+    //results
+    app.get('/results', Index.search);
 }

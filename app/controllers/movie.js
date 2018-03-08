b1b06bea6b0b00c0a155373380c5be2684ab5652
againF <wyffcld@163.com>
2018-03-08 20:52:26 +0800

    增加海报上传、统计功能

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -2,7 +2,8 @@ var _ = require('underscore');
 var Movie = require('../models/movie');
 var Comment = require('../models/comment');
 var Category = require('../models/category');
-
+var fs = require('fs');
+var path = require('path');
 //detail page
 exports.detail = function(req, res) {
     var id = req.params.id;
@@ -57,12 +58,40 @@ exports.update = function(req, res) {
     }
 }
 
+// admin poster
+exports.savePoster = function(req, res, next) {
+    var posterData = req.files.uploadPoster
+    var filePath = posterData.path
+    var originalFilename = posterData.originalFilename
+  
+    if (originalFilename) {
+      fs.readFile(filePath, function(err, data) {
+        var timestamp = Date.now()
+        var type = posterData.type.split('/')[1]
+        var poster = timestamp + '.' + type
+        var newPath = path.join(__dirname, '../../', '/public/upload/' + poster)
+  
+        fs.writeFile(newPath, data, function(err) {
+          req.poster = poster
+          next()
+        })
+      })
+    }
+    else {
+      next()
+    }
+  }
+
 //admin post movie从后台录入页表单提交后电影数据的储存
 exports.save = function(req, res) {
     var id = req.body.movie._id;	
     var movieObj = req.body.movie;//post的movie
     console.log(movieObj)
     var _movie;
+
+    if(req.poster) {
+        movieObj.poster = req.poster;
+    }
     //已经存在数据库中的电影对其更新操作
     if(id) {
         Movie.findById(id, function(err, movie) {
